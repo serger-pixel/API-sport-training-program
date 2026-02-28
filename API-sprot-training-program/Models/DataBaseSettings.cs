a1c23dcd286483031f6eb56596a7ebd80f273d76
Sergey Ermakov <rihlerstudy@gmail.com>
2026-02-28 12:37:22 +0300

    Добавлен класс описания бд

---
+++ API-sprot-training-program/Models/DataBaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace API_sprot_training_program.Models
+{
+    public class DataBaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string CollectionName { get; set; } = null!;
+    }
+}
