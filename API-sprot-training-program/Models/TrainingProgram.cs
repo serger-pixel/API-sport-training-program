74905f76223bc26e9584c05dabb1d61337ad6066
Sergey Ermakov <rihlerstudy@gmail.com>
2026-02-28 12:24:39 +0300

    Добавление атрибутов модели для BSON

--- API-sprot-training-program/Models/TrainingProgram.cs
+++ API-sprot-training-program/Models/TrainingProgram.cs
@@ -1,15 +1,24 @@
-﻿namespace API_sprot_training_program.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace API_sprot_training_program.Models
 {
     public class TrainingProgram
     {
-        public int Id { get; set; }
 
+        [BsonRepresentation(BsonType.ObjectId)]
+        public long Id { get; set; }
+
+        [BsonDefaultValue("Программа")]
         public String Title { get; set; }
 
+        [BsonDefaultValue("Отсутсвует")]
         public String Description { get; set; }
 
+        [BsonDefaultValue(0)]
         public int DurationInWeek { get; set; }
-         
+
+        [BsonDefaultValue(0)]
         public int CntInWeek { get; set; }
 
     }
