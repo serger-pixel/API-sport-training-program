af56f671634ca33f7f2757d194fe8a0620a07c56
Sergey Ermakov <rihlerstudy@gmail.com>
2026-03-07 08:27:50 +0300

    Редактирование параметров запуска и главной программы

--- API-sprot-training-program/Program.cs
+++ API-sprot-training-program/Program.cs
@@ -1,9 +1,15 @@
 
+using API_sprot_training_program.Models;
+using API_sprot_training_program.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<DataBaseSettings>(
+    builder.Configuration.GetSection("TrainingProgramsDatabase"));
+builder.Services.AddSingleton<TrainingProgramService>();
 
 var app = builder.Build();
 
@@ -12,7 +18,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.MapControllers();
 
 app.Run();
