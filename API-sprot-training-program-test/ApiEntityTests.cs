f506dc0098215c35784ca7e78c8b1d1f19d31ec5
Sergey Ermakov <rihlerstudy@gmail.com>
2026-03-13 16:54:16 +0300

    Добавление тестов на добавление 100000 элементов и удаление всех элементов

--- API-sprot-training-program-test/ApiEntityTests.cs
+++ API-sprot-training-program-test/ApiEntityTests.cs
@@ -33,7 +33,66 @@ namespace API_sprot_training_program_test
                     new StringContent(json, Encoding.UTF8, "application/json"));
                 response.EnsureSuccessStatusCode();
             }
+        }
 
+
+        [Fact]
+        public async Task Insert_100000_Entities()
+        {
+            int total = 100000;
+            int parallelRequests = 50;
+
+            var semaphore = new SemaphoreSlim(parallelRequests);
+            var tasks = new List<Task>();
+
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:5276")
+            };
+
+            for (int i = 0; i < total; i++)
+            {
+                int index = i;
+
+                tasks.Add(Task.Run(async () =>
+                {
+                    await semaphore.WaitAsync();
+
+                    try
+                    {
+                        var entity = new DtoCreateUpdate
+                        {
+                            Title = "Навзвание",
+                            Description = "Описание",
+                            CntInWeek = i % 7 + 1,
+                            DurationInWeek = i % 52 + 1
+                        };
+                        var json = JsonSerializer.Serialize(entity);
+
+                        var response = await client.PostAsync(
+                            "/api/programs",
+                            new StringContent(json, Encoding.UTF8, "application/json"));
+                        response.EnsureSuccessStatusCode();
+                    }
+                    finally
+                    {
+                        semaphore.Release();
+                    }
+                }));
+            }
+
+            await Task.WhenAll(tasks);
+
+        }
+
+        [Fact]
+        public async Task Delete_All()
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:5276")
+            };
+            await client.DeleteAsync("/api/programs/all");
         }
 
     }
